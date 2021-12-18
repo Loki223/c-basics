53dc7e1694eb06bef0cb52d7752306be4c0a0a28
Loki223 <96326944+Loki223@users.noreply.github.com>
2021-12-18 15:13:55 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+void main ()
+{
+int x;
+int y;
+int z;
+x=1;
+y=2;
+z=x+y;
+printf("sum of x and y %d",z);
+}
---
+++ sumab.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{int a, b ;
+a=90;
+b=100;
+printf("sum of a and b %d" , a+b );
+
+}
