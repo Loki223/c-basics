7f9f4240bf6767aa9a9984a647337db5b2f184a7
Loki223 <96326944+Loki223@users.noreply.github.com>
2022-01-21 17:44:18 +0530

    Add files via upload

--- main (1).c
+++ main (1).c
@@ -6,23 +6,29 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
 
-// Online C compiler to run C program online
 #include <stdio.h>
 
-int main()
+int
+main ()
 {
-    
-    int times,i,j;
-    scanf("%d",&times);
-    for (i=1;i<=times;i++)
-    {
-        for (j=1;j<=i;j++)
-        {
-        printf("* " );
-        }
-    printf("\n");
-    }
-    
-    
-}
+  int n, largest;
+  scanf ("%d", &n);
+  int i, arr[n];
 
+  for (i = 0; i < n; i++)
+    {
+      scanf ("%d", &arr[i]);
+    }
+  for (i = 0; i < n - 1; i++)	//0 1 2 3 4
+    {
+      if (arr[i] > arr[i + 1])
+	{
+	  largest = arr[i];
+	}
+      else
+	{
+	  largest = arr[i + 1];
+	}
+    }
+  printf ("%d", largest);
+}
